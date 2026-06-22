8e60d2343cc22488c526068da2ac5688eae81f77
Yurii Osadchyi <malicious1986@gmail.com>
2026-06-22 21:24:13 +0100

    fixes

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -9,4 +9,35 @@ resource "aws_ecr_repository" "ecr" {
   tags = {
     Name = var.ecr_name
   }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+resource "aws_ecr_registry_policy" "example" {
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "testpolicy",
+        Effect = "Allow",
+        Principal = {
+          "AWS" : "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action = [
+          "ecr:ReplicateImage"
+        ],
+        Resource = [
+          "arn:${data.aws_partition.current.partition}:ecr:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:repository/*"
+        ]
+      }
+    ]
+  })
 }
