c39ed79a00a4fb3f93760cd324bc11603f0a4e96
Yurii Osadchyi <malicious1986@gmail.com>
2026-06-29 00:34:38 +0100

    done 2

--- django/app/config/settings.py
+++ django/app/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os   
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-y!5ezzc)z4kmb251l__pnjoap-=7z*_6dwuz@brcmort#3k!+-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web']
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1,web"
+).split(",")
 
 
 # Application definition
