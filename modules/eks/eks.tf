84da7c3a31840c650550641888fdcb921aba54f6
Yurii Osadchyi <malicious1986@gmail.com>
2026-07-08 22:52:33 +0100

    Done

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -22,25 +22,21 @@ resource "aws_iam_role_policy_attachment" "eks" {
 }
 
 resource "aws_eks_cluster" "eks" {
-  # Назва кластера
   name     = var.cluster_name
 
-  # ARN IAM-ролі, яка потрібна для керування кластером
   role_arn = aws_iam_role.eks.arn
   
   # Налаштування мережі (VPC)
   vpc_config {
-    endpoint_private_access = true   # Включає приватний доступ до API-сервера
-    endpoint_public_access  = true   # Включає публічний доступ до API-сервера
-    subnet_ids = var.subnet_ids      # Список підмереж, де буде працювати EKS
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    subnet_ids = var.subnet_ids
   }
 
-  # Налаштування доступу до EKS-кластера
   access_config {
-    authentication_mode                         = "API"  # Автентифікація через API
-    bootstrap_cluster_creator_admin_permissions = true   # Надає адміністративні права користувачу, який створив кластер
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
-  # Залежність від IAM-політики для ролі EKS
   depends_on = [aws_iam_role_policy_attachment.eks]
 }
